bdf12269fe17ac46c162cefb356d428466c3b6a2
adsoftsito <adsoft@live.com.mx>
2021-06-15 20:00:56 +0000

    add pagination

--- links/schema.py
+++ links/schema.py
@@ -16,18 +16,31 @@ class VoteType(DjangoObjectType):
         model = Vote
 
 class Query(graphene.ObjectType):
-    links = graphene.List(LinkType, search=graphene.String())
+    links = graphene.List(
+        LinkType, 
+        search=graphene.String(),
+        first=graphene.Int(),
+        skip=graphene.Int(),
+    )
     votes = graphene.List(VoteType)
 
-    def resolve_links(self, info, search=None, **kwargs):
+    def resolve_links(self, info, search=None,first=None, skip=None, **kwargs):
+        qs = Link.objects.all()
+
         if search:
             filter = (
                 Q(url__icontains=search) |
                 Q(description__icontains=search)
             )
-            return Link.objects.filter(filter)
+            qs = qs.filter(filter)
+ 
+        if skip:
+            qs = qs[skip:]
 
-        return Link.objects.all()
+        if first:
+            qs = qs[:first]
+
+        return qs
 
     def resolve_votes(self, info, **kwargs):
         return Vote.objects.all()
